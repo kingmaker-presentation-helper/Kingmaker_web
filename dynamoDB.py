8c57ce59812fde56c5ced4cf08f3d1b511670039
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-12-15 18:54:05 +0900

    FASTAPI

--- dynamoDB.py
+++ dynamoDB.py
@@ -1,7 +1,29 @@
 import boto3
+from botocore.exceptions import ClientError
 
-def get_dynamodb_data():
+# DynamoDB에 연결하기 위한 함수
+def dynamodb_connection():
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('your_table_name')
-    response = table.scan()
-    return response['Items']
+    return dynamodb
+
+# 테이블에서 모든 데이터를 스캔하는 함수
+def scan_table():
+    try:
+        dynamodb = dynamodb_connection()
+        table = dynamodb.Table('test')
+        response = table.scan()
+        return response['Items']
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return None
+
+# 특정 항목을 조회하는 함수 (예: 특정 ID에 해당하는 데이터 조회)
+def get_item(item_key):
+    try:
+        dynamodb = dynamodb_connection()
+        table = dynamodb.Table('test')
+        response = table.get_item(Key=item_key)
+        return response.get('Item', None)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return None
--- fastAPI.py
+++ fastAPI.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-from your_dynamodb_module import get_dynamodb_data
+from dynamoDB import scan_table, get_item
 
 app = FastAPI()
 
-@app.get("/data")
-def read_data():
-    data = get_dynamodb_data()
-    return {"data": data}
+# 모든 데이터를 조회하는 API 엔드포인트
+@app.get("/items")
+def read_items():
+    items = scan_table()
+    return {"items": items}
+
+# 특정 키에 해당하는 항목을 조회하는 API 엔드포인트
+@app.get("/items/{item_id}")
+def read_item(item_id: str):
+    item = get_item({"id": item_id})
+    if item:
+        return {"item": item}
+    return {"error": "Item not found"}
