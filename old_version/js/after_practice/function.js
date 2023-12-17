3639fda0681b7d00797eab7119b8638d0cbfdce0
송윤수 <57437648+HBKVN@users.noreply.github.com>
2023-12-17 16:39:50 +0900

    after_practice 약간

--- old_version/js/after_practice/function.js
+++ old_version/js/after_practice/function.js
@@ -1,10 +1,10 @@
 (function ($) {
     "use strict";
     var session = localStorage.getItem('session_key');
-    if(session === null) {
-        alert("세션이 만료되었습니다.");
-        window.history.back();
-    }
+    // if(session === null) {
+    //     alert("세션이 만료되었습니다.");
+    //     window.history.back();
+    // }
     localStorage.removeItem('session_key');
     console.log('session: '+session);
 
