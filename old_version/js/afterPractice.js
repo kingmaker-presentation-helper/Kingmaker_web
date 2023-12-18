4ba4249e9573fa04e6d7ef0c04e424cdffe60a37
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-12-18 17:19:32 +0900

    프로그레스 바 등 추가

--- old_version/js/afterPractice.js
+++ old_version/js/afterPractice.js
@@ -1,24 +1,29 @@
 // 전역 변수 설정
-var durationStandard = 110;
+var speedStandard = 110;
 
 
 var fillerwordHighStandard = 10;
 var fillerwordMiddleStandard = 5;
 
-var pronunciationHighStandard = 80;
-var pronunciationMiddleStandard = 60;
+var pronunciationHighStandard = 60;
+var pronunciationMiddleStandard = 50;
 
 var highlightedHighStandard = 0.01;
 var highlightedMiddleStandard = 0.005;
 
-
+var EmMiddleStandard = 0.005;
+var EmHighStandard = 0.1;
+// 새로 추가된 전역 변수들
+var standardWordsPerSecond = 1.7; //초당 발화 수의 기준
+var pronunciationScoreMultiplier = 20; //발음 평가 점수에 곱하는 상수 
+var highlightedScoreMultiplier = 1000;//em평가 점수에 곱하는 상수
 
 
 // const sessionkey = localStorage.getItem("session_key")
 const sessionkey = "test"
 
 window.onload = function() {
-    updateDuration();
+    updateSpeed();
     updateFillerword();
     updatePronunciation();
     updateHighlighted();
@@ -26,239 +31,209 @@ window.onload = function() {
     updateKeywords();
 };
 
-// async function updateDuration() {
-//     let data; // 'data' 변수를 외부에 선언
-//     try {
-//         // 'sessionkey' 변수를 URL에 동적으로 삽입
-//         const response = await fetch(`http://127.0.0.1:9000/data/duration/${sessionkey}?session_key=${sessionkey}`);
-//         data = await response.json();
-//     } catch (error) {
-//         console.error('데이터를 가져오는데 실패했습니다.', error);
-//         return; // 오류 발생 시 함수 실행 중단
-//     }
 
-//     const durationElement = document.getElementById("duration");
-
-//     // 'data' 변수를 사용하여 요소의 텍스트 업데이트
-//     if (data && durationElement) {
-
-//         const speedPercentage = document.getElementById("speed-percentage");
-//         const speedProgress = document.getElementById("speed-progress");
-//         // 카드 
-//         durationElement.querySelector('.card-title').innerText = data + "%";
-//         durationElement.querySelector('.card-text').innerText = "평균보다 " + data + "% 빠르게 말해요";
-//         durationElement.querySelector('.additional-info').innerText = "새로운 추가 정보";
-//         // 프로그레스 바 
-//         speedPercentage.innerText = data + "%";
-//         speedProgress.style.width = data + "%";
-//         speedProgress.setAttribute("aria-valuenow", data);
-
-
-
-//         // 색상 업데이트
-//         var colorClass = 'bg-danger';
-//         if (data >= durationMiddleStandard) colorClass = 'bg-warning';
-//         if (data >= durationHighStandard) colorClass = 'bg-primary';
-//         speedProgress.className = `progress-bar ${colorClass}`;
-//         durationElement.className =`card text-white bg-primary h-100 ${colorClass}`;
-//     }
-// }
-
-async function updateDuration() {
-    let data; // 'data' 변수를 외부에 선언
+async function updateSpeed() {
+    let data;
     try {
-        // 'sessionkey' 변수를 URL에 동적으로 삽입
         const response = await fetch(`http://127.0.0.1:9000/function/speech_speed/${sessionkey}?session_key=${sessionkey}`);
         data = await response.json();
     } catch (error) {
         console.error('데이터를 가져오는데 실패했습니다.', error);
-        return; // 오류 발생 시 함수 실행 중단
+        return;
     }
 
-    const durationElement = document.getElementById("duration");
+    const SpeedElement = document.getElementById("Speed");
 
-    // 'data' 변수를 사용하여 요소의 텍스트 업데이트
-    if (data && durationElement) {
-        // 초당 단어 수에 따른 백분율 계산
+
+    if (data && SpeedElement) {
         const standardWordsPerSecond = 1.7;
         const percentage = (data.words_per_second / standardWordsPerSecond) * 100;
 
-        // 카드 및 프로그레스 바 업데이트
-        durationElement.querySelector('.card-title').innerText = `${percentage.toFixed(2)}%`;
-        durationElement.querySelector('.card-text').innerText = "빠르게 말해요";
-        durationElement.querySelector('.additional-info').innerText = `1초에 ${data.words_per_second.toFixed(2)}번의 단어를 발화했습니다`;
+        var colorClass = 'bg-primary';
+        var textContent = "말하는 속도가  적당해요. 청중과 잘 소통하고 있어요!";
+        var progressValue = 100;
+        if (percentage <= speedStandard - 10) {
+            colorClass = 'bg-warning';
+            textContent = "조금 느긋한 편이네요. 천천히 말하는 것도 좋지만, 조금만 속도를 높여보는 건 어떨까요?";
+            progressValue = 50;
+        }
+        
+        if (percentage >= speedStandard + 10) {
+            colorClass = 'bg-danger';
+            textContent = "빠르게 말하고 있어요! 열정이 느껴지지만, 청중이 따라갈 수 있도록 속도를 조금 줄여보세요.";
+            progressValue = 50;
+        }
+
+        SpeedElement.className = `card text-white ${colorClass} h-100`;
+        SpeedElement.querySelector('.card-title').innerText = `${percentage.toFixed(2)}%`;
+        SpeedElement.querySelector('.card-text').innerText = textContent;
+        SpeedElement.querySelector('.additional-info').innerText = `1초에 ${data.words_per_second.toFixed(2)}번의 단어를 발화했습니다`;
 
         const speedProgress = document.getElementById("speed-progress");
-        speedProgress.style.width = `${percentage}%`;
+        const speedPercentage = document.getElementById("speed-percentage");
+        // speedPercentage.innerText = `${percentage.toFixed(2)}%`;
+        speedProgress.style.width = `${progressValue}%`;
         speedProgress.setAttribute("aria-valuenow", percentage);
-
-        // 색상 업데이트(90~110 평균, 90이하 느림, 110이상 빠름. 기준 초당 단어 1.7번)
-        var colorClass = 'bg-primary';
-        var textContent = "평균적인 속도로 발화해요.";
-        if (percentage <= durationStandard - 10) 
-        {
-            colorClass = 'bg-warning';
-            textContent = "평균보다 느리게 발화해요.";}
-
-        if (percentage >= durationStandard + 10){
-            colorClass = 'bg-danger';
-            textContent = "평균보다 빠르게 발화해요.";
-        } 
-
-
-
         speedProgress.className = `progress-bar ${colorClass}`;
-        durationElement.className =`card text-white ${colorClass} h-100`;
-
-        durationElement.querySelector('.card-title').innerText = `${percentage.toFixed(2)}%`;
-        durationElement.querySelector('.card-text').innerText = textContent;
-        durationElement.querySelector('.additional-info').innerText = `1초에 ${data.words_per_second.toFixed(2)}번의 단어를 발화했습니다`;
     }
 }
 
 
 
 async function updateFillerword() {
-    let data; // 'data' 변수를 외부에 선언
+    let data;
     try {
-        // 'sessionkey' 변수를 URL에 동적으로 삽입
         const response = await fetch(`http://127.0.0.1:9000/function/fillerword/${sessionkey}?session_key=${sessionkey}`);
         data = await response.json();
     } catch (error) {
         console.error('데이터를 가져오는데 실패했습니다.', error);
-        return; // 오류 발생 시 함수 실행 중단
+        return;
     }
 
     if (data) {
-        // 총합을 계산
         let total = 0;
         for (const key in data) {
             total += data[key];
         }
-    
-        // 개별 항목의 문자열을 생성
+
         let details = "";
         for (const key in data) {
             details += `${key}: ${data[key]}번\n`;
         }
-    
-        // 요소의 텍스트 업데이트
+
         const fillerwordElement = document.getElementById("fillerword");
-        fillerwordElement.querySelector('.card-title').innerText = total + "개";
-        fillerwordElement.querySelector('.card-text').innerText = "평균보다 필러워드를 자주 사용해요";
-        fillerwordElement.querySelector('.additional-info').innerText = details.trim();
-        // 필러워드 프로그레스 바 업데이트
-        const fillerwordPercentage = document.getElementById("fillerword-percentage");
         const fillerwordProgress = document.getElementById("fillerword-progress");
+        const fillerwordPercentage = document.getElementById("fillerword-percentage");
 
-        fillerwordPercentage.innerText = total + "%";
-        fillerwordProgress.style.width = total + "%";
-        fillerwordProgress.setAttribute("aria-valuenow", total);
+        var colorClass, textContent, progressBarValue;
+        if (total <= fillerwordMiddleStandard) {
+            colorClass = 'bg-primary';
+            textContent = "필러워드 사용이 적어요. 좋습니다!";
+            progressValue = 100;
+        } else if (total <= fillerwordHighStandard) {
+            colorClass = 'bg-warning';
+            textContent = "필러워드 사용이 보통입니다. 개선이 가능해요!";
+            progressValue = 60;
+        } else {
+            colorClass = 'bg-danger';
+            textContent = "필러워드 사용이 많습니다. 주의가 필요해요!";
+            progressBarValue = 25;
+        }
 
-        // 색상 업데이트
-        var colorClass = 'bg-danger';
-        if (total >= fillerwordMiddleStandard) colorClass = 'bg-warning';
-        if (total >= fillerwordHighStandard) colorClass = 'bg-primary';
+        fillerwordElement.querySelector('.card-title').innerText = total + "개";
+        fillerwordElement.querySelector('.card-text').innerText = textContent;
+        fillerwordElement.querySelector('.additional-info').innerText = details.trim();
+
         fillerwordProgress.className = `progress-bar ${colorClass}`;
-        fillerwordElement.className =`card text-white bg-primary h-100 ${colorClass}`;
+        fillerwordElement.className = `card text-white ${colorClass} h-100`;
+
+        // fillerwordPercentage.innerText = `${progressBarValue.toFixed(2)}%`;
+        fillerwordProgress.style.width = `${progressValue}%`;
+        fillerwordProgress.setAttribute("aria-valuenow", progressBarValue);
     }
-    
 }
 
 
+
 async function updatePronunciation() {
-    let data; // 'data' 변수를 외부에 선언
+    let data;
     try {
-        // 'sessionkey' 변수를 URL에 동적으로 삽입
         const response = await fetch(`http://127.0.0.1:9000/data/pronunciation/${sessionkey}?session_key=${sessionkey}`);
         data = await response.json();
     } catch (error) {
         console.error('데이터를 가져오는데 실패했습니다.', error);
-        return; // 오류 발생 시 함수 실행 중단
+        return;
     }
-
-
-
 
     if (data) {
-        /************************************************************* */
-        const score = data * 20; // 점수 계산 예시
-        /************************************************************* */
+        const score = data * pronunciationScoreMultiplier;
 
-        // 'data' 변수를 사용하여 요소의 텍스트 업데이트
         const pronunciationElement = document.getElementById("pronunciation");
-        pronunciationElement.querySelector('.card-title').innerText = score + "점";
-        pronunciationElement.querySelector('.card-text').innerText = "평균보다 " + data + "% 빠르게 말해요";
+        const pronunciationProgress = document.getElementById("pronunciation-progress");
+        const pronunciationPercentage = document.getElementById("pronunciation-percentage");
+
+        var colorClass, textContent, progressBarValue;
+        if (score >= pronunciationHighStandard) {
+            colorClass = 'bg-primary';
+            textContent = "발음이 매우 좋아요!";
+            progressBarValue = 100;
+        } else if (score >= pronunciationMiddleStandard) {
+            colorClass = 'bg-warning';
+            textContent = 60;
+            progressBarValue = score;
+        } else {
+            colorClass = 'bg-danger';
+            textContent = "발음이 개선이 필요해요!";
+            progressBarValue = 25;
+        }
+
+        pronunciationElement.querySelector('.card-title').innerText = score.toFixed(2) + "점";
+        pronunciationElement.querySelector('.card-text').innerText = textContent;
         pronunciationElement.querySelector('.additional-info').innerText = "평균점수는 50점 이에요";
 
-        // 발음점수 프로그레스 바 업데이트
-        const pronunciationPercentage = document.getElementById("pronunciation-percentage");
-        const pronunciationProgress = document.getElementById("pronunciation-progress");
-        pronunciationPercentage.innerText = score + "%";
-        pronunciationProgress.style.width = score + "%";
-        pronunciationProgress.setAttribute("aria-valuenow", score);
-
-
-         // 색상 업데이트
-        var colorClass = 'bg-danger';
-        if (score >= pronunciationMiddleStandard) colorClass = 'bg-warning';
-        if (score >= pronunciationHighStandard) colorClass = 'bg-primary';
         pronunciationProgress.className = `progress-bar ${colorClass}`;
-        pronunciationElement.className =`card text-white bg-primary h-100 ${colorClass}`;
+        pronunciationElement.className =`card text-white ${colorClass} h-100`;
+
+        // pronunciationPercentage.innerText = `${progressBarValue.toFixed(2)}%`;
+        pronunciationProgress.style.width = `${progressBarValue}%`;
+        pronunciationProgress.setAttribute("aria-valuenow", progressBarValue);
     }
 }
+
 
 async function updateHighlighted() {
     let data;
     try {
         const response = await fetch(`http://127.0.0.1:9000/data/highlight/${sessionkey}?session_key=${sessionkey}`);
         data = await response.json();
-        // console.log("받은 데이터:", data);
     } catch (error) {
         console.error('데이터를 가져오는데 실패했습니다.', error);
         return;
     }
 
-    const highlightedElement = document.getElementById("highlighted");
+    if (data) {
+        const sortedWords = data.comparison_words.sort((a, b) => b.em_score - a.em_score);
+        const topWords = sortedWords.slice(0, 3); // 상위 3개 단어 선택
 
-    if (data && highlightedElement) {
-        // em_score가 0이 아닌 항목 필터링
-        const filteredWords = data.comparison_words.filter(word => word.em_score > 0);
+        const averageEmScore = topWords.reduce((sum, word) => sum + word.em_score, 0) / topWords.length;
+        const score = averageEmScore.toFixed(4) * highlightedScoreMultiplier;
 
-        // 평균 em_score 계산
-        const averageEmScore = filteredWords.reduce((sum, word) => sum + word.em_score, 0) / filteredWords.length;
+        const wordsList = topWords.map(word => word.word).join(", ");
 
-        // em_score가 0이 아닌 단어들 목록 생성
-        const wordsList = filteredWords.map(word => word.word).join(", ");
-
-        /************************************************************* */
-        const score = averageEmScore.toFixed(4) * 1000;
-        /************************************************************* */
-
-        // 요소의 텍스트 업데이트
-        highlightedElement.querySelector('.card-title').innerText = score+ "점";
-        highlightedElement.querySelector('.card-text').innerText = "키워드를 잘 강조했어요!";
-        highlightedElement.querySelector('.additional-info').innerText = `${wordsList} 키워드를 위주로 발표했어요!`;
-
-
-
-        // 키워드강조 프로그레스 바 업데이트
-        const highlightedPercentage = document.getElementById("highlighted-percentage");
+        const highlightedElement = document.getElementById("highlighted");
         const highlightedProgress = document.getElementById("highlighted-progress");
-        highlightedPercentage.innerText = score + "%";
-        highlightedProgress.style.width = score + "%";
-        highlightedProgress.setAttribute("aria-valuenow", score);
+        const highlightedPercentage = document.getElementById("highlighted-percentage");
 
+        var colorClass, textContent, progressBarValue;
+        if (score >= highlightedHighStandard) {
+            colorClass = 'bg-primary';
+            textContent = "키워드를 매우 잘 강조했어요!";
+            progressBarValue = 100;
+        } else if (score >= highlightedMiddleStandard) {
+            colorClass = 'bg-warning';
+            textContent = "60";
+            progressBarValue = score;
+        } else {
+            colorClass = 'bg-danger';
+            textContent = "키워드 강조가 부족해요. 개선이 필요합니다!";
+            progressBarValue = 25;
+        }
 
-        // 색상 업데이트
-        var colorClass = 'bg-danger';
-        if (score >= highlightedMiddleStandard) colorClass = 'bg-warning';
-        if (score >= highlightedHighStandard) colorClass = 'bg-primary';
+        highlightedElement.querySelector('.card-title').innerText = score.toFixed(2) + "점";
+        highlightedElement.querySelector('.card-text').innerText = textContent;
+        
+        // .additional-info에 부드럽게 업데이트
+        highlightedElement.querySelector('.additional-info').innerText = `특히 '${wordsList}'에 대한 언급이 두드러졌어요. 관련 내용을 잘 전달하고 있네요!`;
+
         highlightedProgress.className = `progress-bar ${colorClass}`;
-        highlightedElement.className =`card text-white bg-primary h-100 ${colorClass}`;
-    }
+        highlightedElement.className = `card text-white ${colorClass} h-100`;
 
+        // highlightedPercentage.innerText = `${progressBarValue.toFixed(2)}%`;
+        highlightedProgress.style.width = `${progressBarValue.toFixed(2)}%`;
+        highlightedProgress.setAttribute("aria-valuenow", progressBarValue);
+    }
 }
+
 
 
 async function updatePose() {
@@ -331,10 +306,10 @@ async function updateKeywords() {
 
                 // 카드 배경색 설정
                 let cardClass = 'bg-danger'; // 낮은 em_score (기본값)
-                if (item.em_score > 0.005) {
+                if (item.em_score > EmMiddleStandard) {
                     cardClass = 'bg-warning'; // 중간 em_score
                 } 
-                if (item.em_score > 0.01) {
+                if (item.em_score > EmHighStandard) {
                     cardClass = 'bg-primary'; // 높은 em_score
                 }
 
