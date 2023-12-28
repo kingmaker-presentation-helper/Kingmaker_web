a9584d7d390002b1078bf721e379650abcb624a3
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-12-28 18:14:24 +0900

    결과표 js 합침

--- old_version/js/afterPractice.js
+++ old_version/js/afterPractice.js
@@ -1,3 +1,37 @@
+(function ($) {
+    "use strict";
+
+    var sessionkey
+    // Calender
+    $(document).ready(function() {
+        $('#calender').datetimepicker({
+            inline: true,
+            format: 'L',
+        });
+    });
+
+    // 발표 연습 하러가기 버튼
+    $('#goPractice').on('click', function(){
+        window.location.href = 'before_practice.html';
+    });
+    
+    // 연습결과 확인하기 버튼
+    $('#checkPractice').on('click', function(){
+        sessionkey = $('#session').val();
+        if(sessionkey.trim() === ""){
+            alert("세션 키를 입력해주세요.");
+        } else {
+            localStorage.setItem('sessionkey', sessionkey);
+            window.location.href = 'expected_question.html';
+        }
+    });
+
+    
+
+})(jQuery);
+
+
+
 // 전역 변수 설정
 var finalScore = 100;
 
