cfe4824d946ba6ff7788436aaada2594be0abee3
송윤수 <57437648+HBKVN@users.noreply.github.com>
2024-01-09 06:55:20 +0900

    ppt 띄우기
    middle_practice에서 ppt 띄웠습니다

--- old_version/js/middle_practice/function.js
+++ old_version/js/middle_practice/function.js
@@ -1,130 +1,50 @@
-// // (function ($) {
-// //     var sessionkey = "";
-// //         $.ajax({
-// //             url: "http://localhost:9000/function/sessionkey",
-// //             type: "GET",
-// //             dataType: "json",
-// //             success: function(data) {
-// //                 sessionkey = data.sessionkey;
-// //                 console.log(sessionkey);
-// //             },
-// //             error: function(request, status, error) {
-// //                 console.log("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
-// //             }
-// //         });
-    
-// //     "use strict";
-// //     var userDataString = localStorage.getItem('userData');
-// //     var userData = JSON.parse(userDataString);
-    
-// //     userData.sessionKey = sessionkey;
-// //     console.log(userData)
-// //     // userData 내용물
-// //     // userData = {
-// //     //     month: 월,
-// //     //     day: 일,
-// //     //     title: 제목,
-// //     //     keyword: 키워드
-// //     //     ppt: ppt 유무
-// //     // };
-    
-// //     if(userDataString){
-// //         // ppt 업로드 안했으면 ppt 부분 삭제합니다.
-// //         var isppt = userData.ppt;
-// //         if(!isppt){$('#ppt').remove();}
+var sessionkey = localStorage.getItem('sessionkey');
 
-// //         $("#title").text(userData.title);
-// //         $('#date').text(userData.month+'월 '+userData.day+'일');
-// //         // localStorage.removeItem('userData');
-// //         console.log('날짜: '+userData.month+'월 '+userData.day+'일\n제목: '
-// //             +userData.title+' 키워드: '+userData.keyword+'\nppt 유무: '+isppt);
-// //         console.log(userDataString)
-// //         // userDataString의 타입 log
-// //         console.log(typeof(userDataString));
-// //         // localhost:9000/function/info로 post로 정보 전송
-// //         $.ajax({
-// //             url: 'http://localhost:9000/function/info/',  
-// //             type: 'POST',
-// //             contentType: 'application/json',
-// //             data: JSON.stringify(userData), // userData를 JSON 문자열로 변환
-// //             success: function(data){
-// //                 console.log(data);
-// //             },
-// //             error: function(err){
-// //                 console.log(err);
-// //             }
-// //         });
-// //     }
-// //     else{
-// //         alert("발표 데이터가 없습니다!");
-// //         window.history.back();
-// //     }
+window.onload = async function() {
+    await Promise.all([
+        updatePPT(),
+    ]);
+};
 
+async function updatePPT(){
+    try {
+        const response = await fetch(`http://localhost:9000/function/ppt_img/${sessionkey}`);
+        const imagesData = await response.json();
 
-    
-// // })(jQuery);
+        console.log('imagesData: ',imagesData);
 
-// "use strict";
+        const carouselInner = document.querySelector('.carousel-inner');
+        const carouselIndicators = document.querySelector('.carousel-indicators');
 
-// // async function getSessionKey() {
-// //     try {
-// //         const response = await $.ajax({
-// //             url: "http://localhost:9000/function/sessionkey",
-// //             type: "GET",
-// //             dataType: "json",
-// //         });
+        imagesData.images.forEach((imageData, index) =>{
+            const imageUrl = `data:image/png;base64, ${imageData.data}`;
+            const imgElement = new Image();
+            imgElement.src = imageUrl;
+            imgElement.alt = `Image ${index + 1}`;
+            imgElement.className = 'd-block w-100';
 
-// //         return response.sessionkey;
-// //     } catch (error) {
-// //         console.log("Error getting session key:", error);
-// //         throw error; // 예외 처리를 원하는 방식으로 진행하거나 상위로 던집니다.
-// //     }
-// // }
+            const carouselItem = document.createElement('div');
+            carouselItem.className = 'carousel-item';
+            if (index === 0) carouselItem.classList.add('active');
 
-// async function main() {
-//     // var sessionkey = await getSessionKey();
-//     console.log(sessionkey);
+            carouselItem.appendChild(imgElement);
 
-//     var userDataString = localStorage.getItem('userData');
-//     var userData = JSON.parse(userDataString);
+            const caption = document.createElement('div');
+            caption.className = 'carousel-caption d-none d-md-block';
+            carouselItem.appendChild(caption);
 
-//     userData.sessionKey = sessionkey;
-//     console.log(userData);
+            const indicator = document.createElement('button');
+            indicator.setAttribute('type', 'button');
+            indicator.setAttribute('data-bs-target', '#carouselExampleCaptions');
+            indicator.setAttribute('data-bs-slide-to', index);
+            if(index === 0) indicator.classList.add('active');
 
-//     // 이후에 userData를 사용하여 다른 작업 수행
-//     if(userDataString){
-//         // ppt 업로드 안했으면 ppt 부분 삭제합니다.
-//         var isppt = userData.ppt;
-//         if(!isppt){$('#ppt').remove();}
+            carouselInner.appendChild(carouselItem);
+            carouselIndicators.appendChild(indicator);
+        });
 
-//         $("#title").text(userData.title);
-//         $('#date').text(userData.month+'월 '+userData.day+'일');
-//         // localStorage.removeItem('userData');
-//         console.log('날짜: '+userData.month+'월 '+userData.day+'일\n제목: '
-//             +userData.title+' 키워드: '+userData.keyword+'\nppt 유무: '+isppt);
-//         console.log(userDataString)
-//         // userDataString의 타입 log
-//         console.log(typeof(userDataString));
-//         // localhost:9000/function/info로 post로 정보 전송
-//         $.ajax({
-//             url: 'http://localhost:9000/function/info/',  
-//             type: 'POST',
-//             contentType: 'application/json',
-//             data: JSON.stringify(userData), // userData를 JSON 문자열로 변환
-//             success: function(data){
-//                 console.log(data);
-//             },
-//             error: function(err){
-//                 console.log(err);
-//             }
-//         });
-//     }
-//     else{
-//         alert("발표 데이터가 없습니다!");
-//         window.history.back();
-//     }
-// }
-
-
-// // main 함수 호출
-// main();
+    } catch (error) {
+        console.error('Error in updatePPT: ', error);
+        return;
+    }
+}
