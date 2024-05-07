3a2fbd2e9a75c44be27766902b0821e1802238a0
송윤수 <57437648+HBKVN@users.noreply.github.com>
2024-05-07 17:28:41 +0900

    delete unnecessary files
    delete react_old & tmp

--- kingmaker_react_old/src/components/Layout.js
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }) => {
-    return <div style={{ paddingTop: "30px", paddingLeft: "285px" }}>
-        <div style={{ margin: "0 43px" }}>
-            {children}
-        </div>
-    </div>
-};
-
-export default Layout;
